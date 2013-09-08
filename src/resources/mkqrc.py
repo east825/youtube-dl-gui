bbfd0a1bf91a145ec9158bb48c06e1efa0d855a5
Mikhail Golubev <qsolo825@gmail.com>
2013-09-08 19:05:18 +0400

    Update mkqrt.py script, add more icons

--- src/resources/mkqrc.py
+++ src/resources/mkqrc.py
@@ -9,20 +9,22 @@ import argparse
 import lxml.etree as et
 
 
-def create_qrc_file(path):
-    dirname = os.path.basename(path)
+def create_qrc_file(subdir):
     qresource_elt = et.Element('qresource')
-    for child in os.listdir(path):
-        child = os.path.join(path, child)
-        if not os.path.isfile(child):
+    for child in os.listdir(subdir):
+        path = os.path.join(subdir, child)
+        if not os.path.isfile(path):
             continue
-        alias, _ = os.path.splitext(os.path.basename(child))
+        print('File found: {}'.format(child))
+        alias, _ = os.path.splitext(child)
         file_elt = et.SubElement(qresource_elt, 'file', {'alias': alias})
-        file_elt.text = os.path.relpath(child, os.getcwd())
+        file_elt.text = os.path.relpath(path, os.getcwd())
     if len(qresource_elt):
         root = et.Element('RCC', {'version': '1.0'})
         root.append(qresource_elt)
-        with open(dirname + '.qrc', 'w') as f:
+        qrc_file_name = os.path.basename(subdir) + '.qrc'
+        with open(qrc_file_name, 'w') as f:
+            print('Writing to {}'.format(qrc_file_name))
             f.write(et.tostring(root,
                                 encoding='utf-8',
                                 pretty_print=True,
@@ -31,16 +33,18 @@ def create_qrc_file(path):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', nargs='?', default=os.curdir,
-                        help='dirpath containing resource folders')
+                        help='directory containing resources in separate folders')
 
     args = parser.parse_args()
     if not os.path.isdir(args.dir):
-        raise Exception('"{}" is not a dirpath'.format(args.dir))
-    path = os.path.abspath(args.dir)
-    for child in os.listdir(path):
-        child = os.path.join(path, child)
+        raise Exception('"{}" is not a directory'.format(args.dir))
+
+    dir_path = os.path.abspath(args.dir)
+    for child in os.listdir(dir_path):
+        child = os.path.join(dir_path, child)
         if os.path.isdir(child):
-            create_qrc_file(os.path.join(path, child))
+            print('Proceeding subdirectory: {}'.format(child))
+            create_qrc_file(child)
 
 
 if __name__ == '__main__':
